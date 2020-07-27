6b6d0b95423c90b80e014ff59b18ab91595a4547
Kuba <kubagorka14@gmail.com>
2020-07-27 20:38:14 +0200

    added saving rooms data to database

--- src/components/Coding/Coding.js
+++ src/components/Coding/Coding.js
@@ -1,12 +1,47 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 import styles from "./Coding.module.css";
 import SideBar from "../SideBar/SideBar";
+import { useLocation } from "react-router-dom";
 const Coding = () => {
+  const roomName = useLocation().pathname.substr(1);
+  let timer = useRef();
+  const [roomData, setRoomData] = useState({
+    version: 0,
+    data: 0,
+    value: "",
+  });
+
+  //Fetch data from the server
+  useEffect(() => {
+    fetch(`https://sharecodetogether.firebaseio.com/rooms/${roomName}.json`)
+      .then((response) => response.json())
+      .then((data) =>
+        setRoomData({ version: data.version, data: 0, value: data.value })
+      );
+  }, [roomName]);
+  //Save data to server
+  useEffect(() => {  
+    clearTimeout(timer.current);
+    timer.current = setTimeout(() => {
+      fetch(`https://sharecodetogether.firebaseio.com/rooms/${roomName}.json`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(roomData),
+      });
+    }, 500);
+  }, [roomData, roomName]);
+
   return (
     <div className={styles.code}>
       <textarea
+        value={roomData.value}
         className={styles.codeText}
         placeholder="Write your code here. People in the same room can see code as you type it!"
+        onChange={(event) => {
+          setRoomData({ ...roomData, value: event.target.value });
+        }}
       ></textarea>
       <SideBar />
     </div>
